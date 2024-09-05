b542d2e0e372efd8e5591e14abe636c6813e34d8
Sunil Kumar <sunil@example.com>
2024-09-05 14:20:37 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,jsonify,request
+import ipl
+import jugaad
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello world"
+
+@app.route('/api/teams')
+def teams():
+    teams = ipl.teamsapi()
+    return jsonify(teams)
+
+@app.route('/api/teamvteam')
+def teamvteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+
+    response = ipl.teamVteam(team1,team2)
+    # print(response)
+    return jsonify(response)
+
+@app.route('/api/team-record')
+def team_record():
+    team_name = request.args.get('team')
+    print(f"Team name received: {team_name}")
+    if not team_name:
+        return jsonify({"error": "Team name not provided"}), 400
+
+    response = jugaad.teamAPI(team_name)
+    return response
+
+@app.route('/api/batting-record')
+def batting_record():
+    batsman_name = request.args.get('batsman')
+    response = jugaad.batsmanAPI(batsman_name)
+    return response
+
+@app.route('/api/bowling-record')
+def bowling_record():
+    bowler_name = request.args.get('bowler')
+    response = jugaad.bowlerAPI(bowler_name)
+    return response
+
+
+app.run(debug=True)
